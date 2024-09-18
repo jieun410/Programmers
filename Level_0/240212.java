bcd5f19bbb1815b421db15301695db65c9577f1d
jieun410 <easyeun410@gmail.com>
2024-09-18 21:02:18 +0900

    폴더 생성

---
+++ Level_0/240212.java
@@ -0,0 +1,17 @@
+// ## [PCCE 기출문제]2번 / 피타고라스의 정리
+// ## https://school.programmers.co.kr/learn/courses/30/lessons/250132
+
+
+// import java.util.Scanner;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int c = sc.nextInt();
+
+//         int b_square = (c*c) - (a*a);
+
+//         System.out.println(b_square);
+//     }
+// }
