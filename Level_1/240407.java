a7beb00a585eedd0cc5332b8b7845ebf33cd2dc5
jieun410 <easyeun410@gmail.com>
2024-09-22 23:10:10 +0900

    Lv.1_평균 구하기

---
+++ Level_1/240407.java
@@ -0,0 +1,13 @@
+// // ## 평균구하기
+// // ## https://school.programmers.co.kr/learn/courses/30/lessons/12944
+// class Solution {
+//     public double solution(int[] arr) {
+//         double answer = 0;
+//         int total = 0;
+//         for(int i = 0; i < arr.length; i++) {
+//             total += arr[i];
+//         }
+//         answer = (double) total / arr.length;
+//         return answer;
+//     }
+// }
