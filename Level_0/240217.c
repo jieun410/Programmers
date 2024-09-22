c5508c89aa2edc788d8ccb43c53feacc63085af4
jieun410 <easyeun410@gmail.com>
2024-09-22 22:58:52 +0900

    Update 240217.c

--- Level_0/240217.c
+++ Level_0/240217.c
@@ -24,3 +24,20 @@ int solution(int num1, int num2) {
     answer = num1 % num2;
     return answer;
 }
+
+// ## 숫자 비교하기
+// ## https://school.programmers.co.kr/learn/courses/30/lessons/120807
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    if(num1 == num2) {
+        answer = 1;
+    }
+    else {
+        answer = -1;
+    }
+    return answer;
+}
