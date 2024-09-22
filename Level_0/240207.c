c8e64c14ae15b7cfdc604f3fd32372c3f6ae9d04
jieun410 <easyeun410@gmail.com>
2024-09-22 22:43:08 +0900

    Update 240207.c

--- Level_0/240207.c
+++ Level_0/240207.c
@@ -9,3 +9,16 @@ int main(void) {
     printf("%s", s1);
     return 0;
 }
+
+//a와 b 출력하기
+//https://school.programmers.co.kr/learn/courses/30/lessons/181951
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    printf("a = %d\n", a);
+    printf("b = %d", b);
+    return 0;
+}
