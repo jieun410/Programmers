6f2e9fea58e33cf80997d9a790cbcaebb1035e1e
jieun410 <easyeun410@gmail.com>
2024-10-01 19:58:36 +0900

    Lv1.자릿수 더하기

---
+++ Level_1/C/241001.c
@@ -0,0 +1,18 @@
+// ## 자릿수 더하기
+// ## https://school.programmers.co.kr/learn/courses/30/lessons/12931
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int sum = 0;
+    scanf("%d", &n);
+
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    
+    return sum;
+}
