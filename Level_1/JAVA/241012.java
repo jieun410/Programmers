5ea73b88cbedb7235a61ee5c868ab0a02eeace7b
Jieun Lee <easyeun410@gmail.com>
2024-10-13 00:04:31 +0900

    Lv1.정수 내림차순 배치

---
+++ Level_1/JAVA/241012.java
@@ -0,0 +1,17 @@
+// ## 정수 내림차순으로 배치하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12933
+
+// import java.util.*;
+ 
+// class Solution {
+//   public long solution(long n) {
+//         String[] list = String.valueOf(n).split("");
+//         Arrays.sort(list);
+ 
+//         StringBuilder sb = new StringBuilder();
+//         for (String aList : list) sb.append(aList);
+ 
+//         return Long.parseLong(sb.reverse().toString());
+//   }
+// }
+
